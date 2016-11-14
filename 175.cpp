e8e77317d06ef1a9e7750804f7b8a20887426cf8
root <weskiller@vip.qq.com>
2016-11-14 14:01:02 +0800

    Add Subject

---
+++ 175.cpp
@@ -0,0 +1,4 @@
+//Subject: https://leetcode.com/problems/combine-two-tables
+select p.FirstName, p.LastName, a.City, a.State 
+from Person as p left outer join Address as a 
+on p.PersonId = a.PersonId;
