cc96728164de167770239b66462b38a0b513c98a
mic weskiller <mic weskiller>
2017-05-19 21:04:59 +0800

    Signed-off-by: mic weskiller <mic weskiller>

---
+++ tmp/test.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	vector<int> findDuplicates(vector<int>& nums) {
+		vector<int> result;
+		vector<int> v(nums.size(), 1);
+		for (auto x : nums) {
+			v[x - 1] -= 1;
+			if (v[x - 1] == -1)
+				result.push_back(x);
+		}
+		return result;
+	}
+};
